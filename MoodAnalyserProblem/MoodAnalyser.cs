17704adc8ef9d36c59336b9d644ef4ec738c8074
Biru4015 <rambirendra12@gmail.com>
2020-04-15 13:01:08 +0530

    [Birendra ]Adding mood analyser whether mood is sad or any mood

---
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyser
+    {
+        String message;
+        public MoodAnalyser()
+        {
+        }
+        public MoodAnalyser(String message)
+        {
+            this.message = message;
+        }
+
+        public String moodAnalyseMethod()
+        {
+            if (message.Contains("sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+    }
+}
