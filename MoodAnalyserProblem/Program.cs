dd9a54450683a6cff69564f69240ee1caaea86d7
Biru4015 <rambirendra12@gmail.com>
2020-04-15 13:38:53 +0530

    [Birendra ]Adding mood analyser whether mood is null it returns HAAPY

--- MoodAnalyserProblem/Program.cs
+++ MoodAnalyserProblem/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MoodAnalyserProblem
 {
+    /// <summary>
+    /// This  class contains the code of main method
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// This is  main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to mood Analyser Problem");
