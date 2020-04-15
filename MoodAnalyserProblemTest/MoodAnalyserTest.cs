136a0c31a9213d9d683d396c57a126602c12326e
Biru4015 <rambirendra12@gmail.com>
2020-04-15 13:39:21 +0530

    [Birendra ]Adding mood analyser whether mood is null it returns HAPPY for testing

--- MoodAnalyserProblemTest/MoodAnalyserTest.cs
+++ MoodAnalyserProblemTest/MoodAnalyserTest.cs
@@ -4,26 +4,42 @@ using System.Text;
 
 namespace MoodAnalyserProblemTest
 {
+    /// <summary>
+    /// This class contains the code of mood analyser test
+    /// </summary>
     public class MoodAnalyser
     {
 
         String message;
-
-        public MoodAnalyser()
-        {
-        }
 
         public MoodAnalyser(String message)
         {
             this.message = message;
         }
 
-        public String moodAnalyseMethod()
+        public String MoodAnalyseMethod(String message)
         {
-            if (message.Contains("sad"))
-                return "SAD";
-            else
+            this.message = message;
+            return MoodAnalyseMethod();
+        }
+
+        /// <summary>
+        /// This method is created for checking string with another string
+        /// </summary>
+        /// <returns>SAD|HAPPY</returns>
+        public String MoodAnalyseMethod()
+        {
+            try
+            {
+                if (message.Contains("sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch(NullReferenceException)
+            {
                 return "HAPPY";
+            }
         }
     }
 }
--- MoodAnalyserProblemTest/UnitTest1.cs
+++ MoodAnalyserProblemTest/UnitTest1.cs
@@ -2,20 +2,39 @@ using NUnit.Framework;
 
 namespace MoodAnalyserProblemTest
 {
+    /// <summary>
+    /// This class contains the code of testing of mood analyser
+    /// </summary>
     public class Tests
     {
+        /// <summary>
+        /// Purpose of this method is when mood is sad it returns sad
+        /// </summary>
         [Test]
         public void MessageWhenSadResposeTest_RetrunsSad()
         {
             MoodAnalyser mood = new MoodAnalyser("I am sad in sad mood");
-            Assert.AreEqual("SAD", mood.moodAnalyseMethod());
+            Assert.AreEqual("SAD", mood.MoodAnalyseMethod());
         }
 
+        /// <summary>
+        /// Purpose of this method is when mood is happy it returns happy
+        /// </summary>
         [Test]
         public void MessageWhenAnyMoodTest_ReturnsHappy()
         {
             MoodAnalyser mood = new MoodAnalyser("I am in any mood");
-            Assert.AreEqual("HAPPY",mood.moodAnalyseMethod());
+            Assert.AreEqual("HAPPY",mood.MoodAnalyseMethod());
+        }
+
+        /// <summary>
+        /// Purpose of this method is when gievn null it return happy
+        /// </summary>
+        [Test]
+        public void MessageNullTest_ReturnsHappy()
+        {
+            MoodAnalyser mood = new MoodAnalyser(null);
+            Assert.AreEqual("HAPPY", mood.MoodAnalyseMethod());
         }
     }
 }
