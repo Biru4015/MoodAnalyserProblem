7b08b703d7013bf2b929d62aa38c127d9ba530a8
Biru4015 <rambirendra12@gmail.com>
2020-04-16 09:56:07 +0530

    [Birendra ]Adding custom exception when mood is null

---
+++ MoodAnalyserProblem/MoodAnalyserCustomException.cs
@@ -0,0 +1,25 @@
+﻿using DotLiquid.Tags;
+using DotLiquid.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Transactions;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalysisException : Exception
+    {
+        ExceptionType type;
+        public enum ExceptionType
+        {
+            ENTERED_NULL, ENTERED_EMPTY
+        }
+
+        public MoodAnalysisException(ExceptionType type, String message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
