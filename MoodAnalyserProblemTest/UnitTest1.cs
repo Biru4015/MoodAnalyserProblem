f93b9bc9eaeaad03cc28c37171e2885b89034881
Biru4015 <rambirendra12@gmail.com>
2020-04-27 18:34:22 +0530

    [Birendra]Refactor refactored. Test case naming convension

--- MoodAnalyserProblemTest/UnitTest1.cs
+++ MoodAnalyserProblemTest/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace MoodAnalyserProblemTest
         /// Purpose of this method is when mood is sad it returns sad
         /// </summary>
         [Test]
-        public void MessageWhenSadResposeTest_RetrunsSad()
+        public void givenSadMesaage_WhenAnalyse_ShouldReturnSad()
         {
             MoodAnalyser mood = new MoodAnalyser("I am sad in sad mood");
             Assert.AreEqual("SAD", mood.MoodAnalyseMethod());
@@ -21,7 +21,7 @@ namespace MoodAnalyserProblemTest
         /// Purpose of this method is when mood is happy it returns happy
         /// </summary>
         [Test]
-        public void MessageWhenAnyMoodTest_ReturnsHappy()
+        public void givenHappyMesaage_WhenAnalyse_ShouldReturnHappy()
         {
             MoodAnalyser mood = new MoodAnalyser("I am in any mood");
             Assert.AreEqual("HAPPY",mood.MoodAnalyseMethod());
@@ -31,7 +31,7 @@ namespace MoodAnalyserProblemTest
         /// Purpose of this method is when gievn null it return happy
         /// </summary>
         [Test]
-        public void MessageNullTest_ReturnsHappy()
+        public void givenNullMessage_WhenAnalse_shouldReturnsHappy()
         {
             MoodAnalyser mood = new MoodAnalyser(null);
             Assert.AreEqual("HAPPY", mood.MoodAnalyseMethod());
